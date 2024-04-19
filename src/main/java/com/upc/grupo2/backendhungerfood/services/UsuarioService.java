53eb6df7147f66fc43bee8907a728feea363e44b
Alexander <u20211b506@upc.edu.pe>
2024-04-19 10:05:34 -0500

    Implementación de @Transactional, intento 2

--- src/main/java/com/upc/grupo2/backendhungerfood/services/UsuarioService.java
+++ src/main/java/com/upc/grupo2/backendhungerfood/services/UsuarioService.java
@@ -2,9 +2,9 @@ package com.upc.grupo2.backendhungerfood.services;
 
 import com.upc.grupo2.backendhungerfood.entities.Usuario;
 import com.upc.grupo2.backendhungerfood.repository.UsuarioRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
